7681dc37f2ba28a4bae7e55c6bdb4638b0456c04
Yousef <yuossefbakier@gmail.com>
2024-01-30 19:33:49 +0200

    Add Task 100

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,26 @@
+-- Write a SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser ( IN u_id INT )
+BEGIN
+    DECLARE sum_weight INT;
+    DECLARE sum_scores INT;
+
+    SELECT SUM(score * projects.weight) INTO sum_scores
+    FROM corrections INNER JOIN projects
+    ON corrections.project_id = projects.id
+    WHERE user_id = u_id;
+
+    SELECT SUM(projects.weight) INTO sum_weight
+    FROM corrections INNER JOIN projects
+    ON corrections.project_id = projects.id
+    WHERE user_id = u_id;
+
+    UPDATE user
+    SET average_score = sum_scores / sum_weight
+    WHERE id - u_id;
+
+END $$
+
+DELIMITER ;
