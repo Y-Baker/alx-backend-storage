d38273b49617bba927f15ad44cee4bfc53d7be3e
Yousef <yuossefbakier@gmail.com>
2024-02-03 16:39:31 +0200

    Fix Code Style

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -8,8 +8,8 @@ def top_students(mongo_collection):
 
     return mongo_collection.aggregate([
         {"$project": {
-            "name" : "$name",
-            "averageScore" : {"$avg" : "$topics.score"}
+            "name": "$name",
+            "averageScore": {"$avg": "$topics.score"}
         }},
         {"$sort": {
             "averageScore": -1
