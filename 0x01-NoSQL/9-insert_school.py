22e8941c1f080232309f146a90b355e6ef5ec65a
Yousef <yuossefbakier@gmail.com>
2024-02-03 15:07:30 +0200

    Add Task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Task 9"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document in a collection based on kwargs"""
+
+    return (mongo_collection.insert_one(kwargs).inserted_id)
