4752ae8945dcdcc91fec15e6468253b8ebe4ee25
Yousef <yuossefbakier@gmail.com>
2024-03-07 19:49:12 +0200

    Add Files

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python3
-"""redis module"""
-
-import redis
+"""
+web cache and tracker
+"""
 import requests
+import redis
 from functools import wraps
-from typing import Union, Callable, Optional
 
 _redis = redis.Redis()
 
 
-def count_url(method: Callable) -> Callable:
-    """Decorator count the url in last 10 min"""
-
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
     @wraps(method)
     def wrapper(url):
-        """Wrapper for Decorator"""
-        name = f"count:{url}"
-        _redis.incr(name)
-        cached_html = _redis.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-        html = method(url)
-        _redis.setex(f"cached:{url}", 10, html)
-        return html
+        cached_key = "cached:" + url
+        cached_data = _redis.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
 
+        count_key = "count:" + url
+        html = method(url)
+
+        _redis.incr(count_key)
+        _redis.set(cached_key, html)
+        _redis.expire(cached_key, 10)
+        return html
     return wrapper
 
-@count_url
-def get_page(url: str) -> str:
-    """obtain the HTML content of a particular URL and returns it"""
 
+@count_url_access
+def get_page(url: str) -> str:
+    """ Returns HTML content of a url """
     res = requests.get(url)
     return res.text
