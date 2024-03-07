2395471974646588a34b25040b902d505a2a90b5
Yousef <yuossefbakier@gmail.com>
2024-03-07 15:27:36 +0200

    Add Task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,7 @@ import uuid
 from functools import wraps
 from typing import Union, Callable, Optional, Any
 
+
 def count_calls(method: Callable) -> Callable:
     """Decorator that takes a single method and returns a new method"""
     @wraps(method)
@@ -15,12 +16,25 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Decorator that takes a single method and returns a new method"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function"""
+        self._redis.rpush(f"{method.__qualname__}:inputs", str(args))
+        v = method(self, *args, **kwargs)
+        self._redis.rpush(f"{method.__qualname__}:outputs", str(v))
+        return v
+    return wrapper
+
+
 class Cache:
     def __init__(self) -> None:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         rand = str(uuid.uuid4())
         self._redis.set(rand, data)
@@ -41,4 +55,3 @@ class Cache:
         except Exception:
             value = 0
         return value
-
